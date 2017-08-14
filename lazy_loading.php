053aa0c7a64bd8c6417f9f79bf719f80a96bb243
Roman <alarmdemo@ukr.net>
2017-08-14 11:57:29 +0300

    Update lazy_loading.php

--- lazy_loading.php
+++ lazy_loading.php
@@ -5,9 +5,10 @@
  */
 class LazyProduct
 {
+    protected $_product = null;
     protected $_fullName;
     protected $_name;
-    protected $_version;
+    protected $_version;    
 
     /**
      * @param $name
@@ -18,13 +19,27 @@ class LazyProduct
         $this->_name = $name;
         $this->_version = $version;
     }
+    
+    
+    /**
+     * @return string
+     *
+     * Property _fullName initialization after call
+     */
+    public function getProduct()
+    {
+        if(is_null($this->_product)) {
+            $this->_product = // upload from DB big product data
+        }
+        return $this->_product;
+    }
 
     /**
      * @return string
      *
      * Property _fullName initialization after call
      */
-    public function getProductName()
+    public function getProductFullName()
     {
         if(empty($this->_fullName)) {
             $this->_fullName = $this->_name . ' ' . $this->_version;
