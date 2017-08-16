adc7928443aad329563dd3121ba421c6daa2a968
Roman <alarmdemo@ukr.net>
2017-08-16 12:59:11 +0300

    bridge

---
+++ creational/bridge.php
@@ -0,0 +1,50 @@
+
+/**
+ * Implementor
+ */
+interface ProductInterface
+{
+    public function getName($id);
+}
+
+/**
+ * Implementor Product_1
+ */
+class Product_1 implements ProductInterface
+{
+    public function getName($id)
+    {
+        return 'Product 1 Name '. $id;
+    }
+}
+
+/**
+ * Implementor Product_2 
+ */
+class Product_2 implements ProductInterface
+{
+  public function getName($id)
+    {
+        return 'Product 2 Name '. $id;
+    }
+}
+
+/**
+ * Factory
+ */
+class Factory
+{
+    private $_product;
+    
+    public function  __construct(ProductInterface $product)
+    {
+        $this->_product = $product;
+    }
+    
+    public function getName($id)
+    {
+        return $this->_product->getName($id);
+    }
+}
+$factory = new Factory(new Product_2());
+$factory->getName();
