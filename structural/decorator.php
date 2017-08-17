1b0984d19cb78c4d2d606c681e9fabbc7c93e88c
Roman <alarmdemo@ukr.net>
2017-08-17 17:10:43 +0300

    decorator

---
+++ structural/decorator.php
@@ -0,0 +1,43 @@
+/**
+ * Class DecoratorProduct
+ */
+class DecoratorProduct
+{
+    public $_product;
+
+    /**
+     * DecoratorProduct constructor.
+     * @param Product $product
+     */
+    public function __construct(Product &$product)
+    {
+        $this->_product = $product;
+    }
+
+    /**
+     * @return Product
+     */
+    public function stripXss()
+    {
+        // ...
+        return $this->_product;
+    }
+
+    /**
+     * @return Product
+     */
+    public function format()
+    {
+        // ...
+        return $this->_product;
+    }
+
+    /**
+     * @return Product
+     */
+    public function breakLongStrings()
+    {
+        // ...
+        return $this->_product;
+    }
+}
